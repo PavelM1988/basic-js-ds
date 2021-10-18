565b3bbcaf5eaf4ceb4cb0011f7c6f068fd4e5ba
PavelM1988 <79479113+PavelM1988@users.noreply.github.com>
2021-10-18 22:39:21 +0300

    feat: add files

---
+++ src/st-queue.js
@@ -0,0 +1,42 @@
+const {ListNode} = require("../extensions/list-node.js");
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+
+module.exports = class Queue {
+    constructor() {
+        this.queue = null;
+    }
+
+    getUnderlyingList() {
+        return this.queue;
+    }
+
+    enqueue(val) {
+        let node = new ListNode(val);
+        if (this.queue === null) {
+            this.queue = node;
+        } else {
+            let curNode = this.queue;
+            while (curNode.next) {
+                curNode = curNode.next;
+            }
+            curNode.next = node;
+        }
+    }
+
+    dequeue() {
+        let val = this.queue.value;
+        this.queue = this.queue.next;
+        return val;
+    }
+};
---
+++ src/st-stack.js
@@ -0,0 +1,32 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+module.exports = class Stack {
+
+	constructor() {
+		this.st = [];
+	}
+
+	push(element) {
+		this.st.push(element);
+	}
+
+	pop() {
+		return this.st.pop();
+	}
+
+	peek() {
+		return this.st[this.st.length - 1];
+	}
+}
